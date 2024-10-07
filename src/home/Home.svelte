6078775be01ef3ee9b416910251e1c9b60852d33
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-10-07 16:01:07 -0300

    criando arquivo home e main do projeto

---
+++ src/home/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Header from "./Header.svelte";
+    import Main from "./Main.svelte";
+  </script>
+  
+  <div class="bg-gray-100 min-h-screen">
+    <Header />
+    <Main />
+  </div>
---
+++ src/home/Main.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { app } from "../store";
+    import Card from "./Card.svelte";
+  
+  
+  
+  
+  </script>
+  
+  <div class="max-w-4xl mx-auto -mt-8">
+    <main class="animate-up delay-2">
+      <h1 class="sr-only">Trabalhos</h1>
+  
+      <div class="cards space-y-2">
+        {#each $app.projects as project (project.id)}
+          <Card {project} />
+        {/each}
+      </div>
+     
+    </main>
+  </div>
