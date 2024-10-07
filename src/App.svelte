57b3f66822bf2e85cc55d83407a8838d8e1e87d8
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-10-07 16:15:08 -0300

    criando  o arquivo profile e store

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,15 @@
 <script>
   import "./app.css";
+  import Home from "./home/Home.svelte";
+  import Profile from "./profile/Profile.svelte";
+  import Project from "./project/Project.svelte";
+  import { app } from "./store";
 </script>
 
+{#if $app.page == "home"}
+  <Home />
+{:else if $app.page == "profile"}
+  <Profile />
+{:else}
+  <Project />
+{/if}
